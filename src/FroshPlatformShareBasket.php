8e9c4d97a76f4c45d6a01c7a290663bc172823d0
Stefan Poensgen <mail@stefanpoensgen.de>
2019-12-14 20:54:51 +0100

    Use defaultValue from config.xml

--- src/FroshPlatformShareBasket.php
+++ src/FroshPlatformShareBasket.php
@@ -6,23 +6,10 @@ use Doctrine\DBAL\Connection;
 use Doctrine\DBAL\ConnectionException;
 use Doctrine\DBAL\DBALException;
 use Shopware\Core\Framework\Plugin;
-use Shopware\Core\Framework\Plugin\Context\InstallContext;
 use Shopware\Core\Framework\Plugin\Context\UninstallContext;
-use Shopware\Core\System\SystemConfig\SystemConfigService;
 
 class FroshPlatformShareBasket extends Plugin
 {
-    public function install(InstallContext $context): void
-    {
-        $this->setDefaults([
-            'interval' => 6,
-            'email' => true,
-            'facebook' => true,
-            'whatsapp' => true,
-            'webshare' => true,
-        ]);
-    }
-
     /**
      * @throws DBALException
      * @throws ConnectionException
@@ -42,19 +29,5 @@ class FroshPlatformShareBasket extends Plugin
         $connection->executeQuery('DROP TABLE IF EXISTS `frosh_share_basket_line_item`');
         $connection->executeQuery('SET foreign_key_checks = 1;');
         $connection->commit();
-    }
-
-    public function setDefaults(array $data): void
-    {
-        /** @var SystemConfigService $systemConfig */
-        $systemConfig = $this->container->get(SystemConfigService::class);
-        $domain = $this->getName() . '.config.';
-
-        foreach ($data as $key => $value) {
-            $defaultValue = $systemConfig->get($domain . $key);
-            if ($defaultValue === null) {
-                $systemConfig->set($domain . $key, $value);
-            }
-        }
     }
 }
