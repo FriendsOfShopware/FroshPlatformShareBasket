c3618bad70ef2b8ada60d6e7df21f8480bf77a85
Stefan Poensgen <mail@stefanpoensgen.de>
2022-06-01 11:56:25 +0200

    Update static analyze tools

--- bin/static-analyze.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-composer dump-autoload
-php "`dirname \"$0\"`"/phpstan-config-generator.php
-php ../../../dev-ops/analyze/vendor/bin/phpstan analyze --configuration phpstan.neon --autoload-file=../../../vendor/autoload.php src
-php ../../../dev-ops/analyze/vendor/bin/psalm --config=psalm.xml --show-info=false --threads=4
