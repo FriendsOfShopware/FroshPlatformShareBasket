fb5ba6d6008fda89749f463673cb1d5f35640b8e
Jeffrey Boehm <3028277+jeboehm@users.noreply.github.com>
2023-06-05 09:39:41 +0200

    feat: allow to change criteria before cleaning baskets (#24)
    * feat: allow to change criteria before cleaning baskets
    
    * fix: add strict type declaration
    
    ---------
    
    Co-authored-by: Stefan Poensgen <mail@stefanpoensgen.de>

---
+++ src/Core/Content/ShareBasket/Events/ShareBasketCleanupCriteriaEvent.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Frosh\ShareBasket\Core\Content\ShareBasket\Events;
+
+use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
+use Symfony\Contracts\EventDispatcher\Event;
+
+class ShareBasketCleanupCriteriaEvent extends Event
+{
+    public function __construct(
+        private readonly Criteria $criteria,
+    )
+    {
+    }
+
+    public function getCriteria(): Criteria
+    {
+        return $this->criteria;
+    }
+}
