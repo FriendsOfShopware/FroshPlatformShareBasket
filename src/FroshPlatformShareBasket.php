023fe26d578b4ec7b3217217dac77f574509292e
Stefan Poensgen <mail@stefanpoensgen.de>
2024-03-25 11:45:46 +0100

    feat: remove set foreign_key_checks
    (cherry picked from commit 5e2013516d4208fd34f25106bee36a84b25f1dd5)

--- src/FroshPlatformShareBasket.php
+++ src/FroshPlatformShareBasket.php
@@ -18,9 +18,7 @@ class FroshPlatformShareBasket extends Plugin
         }
 
         $connection = $this->container->get(Connection::class);
-        $connection->executeStatement('SET foreign_key_checks = 0;');
-        $connection->executeStatement('DROP TABLE IF EXISTS `frosh_share_basket`');
         $connection->executeStatement('DROP TABLE IF EXISTS `frosh_share_basket_line_item`');
-        $connection->executeStatement('SET foreign_key_checks = 1;');
+        $connection->executeStatement('DROP TABLE IF EXISTS `frosh_share_basket`');
     }
 }
